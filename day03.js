cf58e54e0304dd982b7d972520102467441a22bc
ag <anne@pc3.home>
2019-12-03 09:11:06 +0100

    DsMerge remote-tracking branch 'origin/master'

--- day03.js
+++ day03.js
@@ -4,16 +4,6 @@ var read2 = fs.readFileSync("input03-2.txt");
 var wire1 = read.toString().split(",");
 var wire2= read2.toString().split(",");
 
-/// TEST
-/* 
-wire1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',');
-wire2 = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',');  
-wire1 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'.split(',');
-wire2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'.split(',');
-wire1 = 'R8,U5,L5,D3'.split(',');
-wire2 = 'U7,R6,D4,L4'.split(',')*/
-
-
 const run = {
     x:0,
     y:0,
@@ -60,7 +50,7 @@ const run = {
         this.x = 0
         this.y = 0
     },
-    action2: function(wire){console.log('action2')
+    action2: function(wire){
         wire.forEach( dir => {
             
             way = dir.substring(0,1)
@@ -84,9 +74,7 @@ const run = {
         return this.minVal
     },
     partTwo: function(){
-        console.log('partTwo')
         while(this.intersection.length > 0){
-            console.log('intersection')
             val = this.intersection.shift();
             vir = val.lastIndexOf(",")
             toSearch = val.substring(0,vir)
